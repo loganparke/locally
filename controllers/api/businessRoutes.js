da865ea7d50140d743a3ff827f1c4b4e3777a1c7
Luke Sturgill <lukesturgill@gmail.com>
2022-04-28 16:52:13 -0600

    added buisness routes

--- controllers/api/businessRoutes.js
+++ controllers/api/businessRoutes.js
@@ -1,12 +1,52 @@
 const router = require('express').Router();
 const { User, Business, Review } = require('../../models');
 
-router.get('/', (req, res) => {
-
+//Display businesses on homepage
+router.get('/', async (req, res) => {
+    try {
+        const dbBusinessData = await Business.findAll({
+          include: [
+            {
+              model: Business,
+              attributes: ['filename', 'description']
+            }
+          ]
+        });
+    
+        const businesses = dbBusinessData.map(business =>
+          business.get({ plain: true })
+        );
+        res.render('homepage', {
+          businesses,
+          loggedIn: req.session.loggedIn
+        });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
 });
 
-router.get('/:id', (req, res) => {
-
+//Display a single business when selected
+router.get('/business/:id', async (req, res) => {
+    try {
+        const dbBusinessData = await Business.findByPk(req.params.id, {
+          include: [
+            {
+              model: Business,
+              attributes: [
+                'id',
+                'description'
+              ]
+            }
+          ]
+        });
+    
+        const business = dbBusinessData.get({ plain: true });
+        res.render('business', { business, loggedIn: req.session.loggedIn });
+      } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
 });
 
 router.post('/', (req, res) => {
